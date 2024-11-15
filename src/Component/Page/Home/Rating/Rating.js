d288619f86a78f14a8324f1bd7452a04d5b16c04
DVAN11 <vanduong0108@gmail.com>
2024-11-15 14:50:30 +0700

    lai

--- src/Component/Page/Home/Rating/Rating.js
+++ src/Component/Page/Home/Rating/Rating.js
@@ -39,6 +39,15 @@ const Rating = () => {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
